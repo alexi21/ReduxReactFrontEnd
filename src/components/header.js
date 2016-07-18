743b6aaa95c60f4ec9ca322f7bd4a85cd6ec83d1
Alex Karolis <alexi2@protonmail.com>
2016-07-18 11:34:09 +1000

    header routing

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+
+  renderLinks() {
+    if (this.props.authenticated) {
+      // show link to sign out
+      return (
+        <li className="nav-item">
+          <Link to="/signout" className="nav-link">Sign Out</Link>
+        </li>
+      );
+    } else {
+      // show a link to sign in or sign up
+      return [
+        <li className="nav-item" key={1}>
+          <Link to="/signin" className="nav-link">Sign In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link to="/signup" className="nav-link">Sign Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render () {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign In
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated }
+}
+
+export default connect(mapStateToProps, null)(Header);
